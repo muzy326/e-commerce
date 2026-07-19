c2e0455f30fcc55c0286cc88f1f2baf04bbe5f14
muzy326 <mujeebamohammed8@gmail.com>
2026-07-19 07:32:25 +0300

    Project running successfully on Replit

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,16 +1,35 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, Router } from '@angular/router';
-// import { AdminService } from '../services/admin.service';
+// // import { Injectable } from '@angular/core';
+// // import { CanActivate, Router } from '@angular/router';
+// // import { AdminService } from '../services/admin.service';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
+// // @Injectable({
+// //   providedIn: 'root'
+// // })
+// // export class AdminGuard implements CanActivate {
+// //   constructor(private adminService: AdminService, private router: Router) {}
+
+// //   canActivate(): boolean {
+// //     if (this.adminService.isAdmin()) return true;
+// //     this.router.navigate(['/login']);
+// //     return false;
+// //   }
+// // }
+// import { Injectable } from "@angular/core";
+// import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+// import { AuthService } from "../services/auth.service";
+
+
+// @Injectable()
+
 // export class AdminGuard implements CanActivate {
-//   constructor(private adminService: AdminService, private router: Router) {}
-
-//   canActivate(): boolean {
-//     if (this.adminService.isAdmin()) return true;
-//     this.router.navigate(['/login']);
-//     return false;
-//   }
+//     constructor(private _router: Router, private _authService: AuthService) { }
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+//         if (this._authService.token && this._authService.role === 'Admin') {
+//             return true;
+//         }
+//         else {
+//             this._router.navigate(['/un-authorized']);
+//             return false;
+//         }
+//     }
 // }
